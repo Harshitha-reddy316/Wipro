fdfbb3f1c00001d39e950fad04a3c5352dd7341e
Harshitha-reddy316 <harshithareddykr@gmail.com>
2025-07-16 07:27:55 +0530

    Add files via upload

---
+++ Datatypes/IncrementString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class IncrementString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String ");
+            string input = Console.ReadLine();
+            string result = " ";
+
+            for(int i=0 ; i < input.Length; i++)
+            {
+                char ch = input[i];
+                char incremented = (char)(ch + 1);
+                if (char.IsLower(incremented)){
+                    incremented = char.ToUpper(incremented);
+                }
+                else if(char.IsUpper(incremented)){
+                    incremented = char.ToLower(incremented);
+                }
+                result = result + incremented;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
