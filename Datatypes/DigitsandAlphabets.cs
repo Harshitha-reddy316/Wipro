d13ef48ab9844c0f9929de3c4b0c477d7a1e231a
Harshitha-reddy316 <harshithareddykr@gmail.com>
2025-07-16 07:12:23 +0530

    Add files via upload

---
+++ Datatypes/DigitsandAlphabets.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class DigitsandAlphabets
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter a String : ");
+            string input = Console.ReadLine();
+
+            int alphabetcount = 0;
+            int digitcount = 0;
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLetter(input[i]))
+                {
+                    alphabetcount++;
+
+                }
+                if (char.IsDigit(input[i]))
+                {
+                    digitcount++;
+                }
+            }
+            Console.WriteLine("Number of Alphabets : " + alphabetcount);
+            Console.WriteLine("Number of digits : " + digitcount);
+        }
+    }
+}
