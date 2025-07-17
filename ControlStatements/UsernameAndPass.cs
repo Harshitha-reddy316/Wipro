5ccbcb580853f60fb363dd6996618b15370eb02a
Harshitha-reddy316 <harshithareddykr@gmail.com>
2025-07-17 08:30:13 +0530

    Add files via upload

---
+++ ControlStatements/UsernameAndPass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class UsernameAndPass
+    {
+        static void Main(string[] args)
+        {
+            string CorrectUsername = "harshitha";
+            string CorrectPassword = "Password";
+            int maxchances = 3;
+
+            for(int i=1; i <= maxchances; i++)
+            {
+                Console.WriteLine("Enter your Username");
+                string Username = Console.ReadLine();
+                Console.WriteLine("Enter your Password");
+                string Password = Console.ReadLine();
+                if(Username == CorrectUsername && Password == CorrectPassword)
+                {
+                    Console.WriteLine(" Login Successfully ");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"Enter correct Username and Password , you are left with attempts: {maxchances - i} ");
+                }
+            }
+            Console.WriteLine("You have been rejected");
+        }
+    }
+}
