9bca79dfd6c16f28284b48365052915744e52d52
Harshitha-reddy316 <harshithareddykr@gmail.com>
2025-07-17 10:46:27 +0530

    Add files via upload

---
+++ arrays/ArrayOperations.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ArrayOperations
+    {
+
+        static void Main()
+        {
+            int[] arr = new int[10];
+
+            Console.WriteLine("Enter 10 integers:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < 10 - 1; i++)
+            {
+                for (int j = 0; j < 10 - i - 1; j++)
+                {
+                    if (arr[j] < arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            int sum = 0;
+            int min = arr[0];
+            int max = arr[0];
+
+            for (int i = 0; i < 10; i++)
+            {
+                sum += arr[i];
+                if (arr[i] < min)
+                    min = arr[i];
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            Console.WriteLine("\nElements in descending order:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine("\n\nMinimum value: " + min);
+            Console.WriteLine("Maximum value: " + max);
+            Console.WriteLine("Sum of all elements: " + sum);
+        }
+    }
+
+}
---
+++ arrays/LengthofArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class LengthofArray
+    {
+        static void Main(string[] args)
+            {
+                int[] arr = { 10, 20, 30, 40, 50 };
+                int count = 0;
+
+                foreach (int item in arr)
+                {
+                    count++;
+                }
+
+                Console.WriteLine("Number of elements in the array: " + count);
+            }
+        }
+
+    }
