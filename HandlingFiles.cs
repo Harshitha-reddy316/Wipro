94095b2cd0d66f92a947fd988fd502f848441157
Harshitha-reddy316 <harshithareddykr@gmail.com>
2025-07-31 19:12:27 +0530

    Add files via upload

---
+++ HandlingFiles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace oops
+{
+    class HandlingFiles
+    {
+        static void Main()
+        {
+            try
+            { 
+                
+                Console.Write("Enter file name (example: myfile.txt): ");
+                string fileName = Console.ReadLine();
+
+                
+                Console.Write("Enter the content to write in the file: ");
+                string content = Console.ReadLine();
+
+                
+                File.WriteAllText(fileName, content);
+
+                
+                Console.WriteLine("File created and content saved successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
+
