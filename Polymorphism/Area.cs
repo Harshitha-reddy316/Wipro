4f807dfebf19ae685b6325a83af2aff0298c706e
Harshitha-reddy316 <harshithareddykr@gmail.com>
2025-07-31 18:58:56 +0530

    Add files via upload

---
+++ Polymorphism/Area.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace oops
+{
+    class CalculatingArea
+    {
+        // Area of Circle
+        public double Area(double radius)
+        {
+            return 3.14 * radius * radius;
+        }
+
+        // Area of Rectangle
+        public int Area(int length, int width)
+        {
+            return length * width;
+        }
+
+        // Area of Triangle
+        public double Area(double b, double h)
+        {
+            return 0.5 * b * h;
+        }
+    }
+
+    class Area
+    {
+        static void Main()
+        {
+            CalculatingArea calc = new CalculatingArea();
+
+            Console.WriteLine("Area of Circle: " + calc.Area(5.0));
+            Console.WriteLine("Area of Rectangle: " + calc.Area(4, 6));
+            Console.WriteLine("Area of Triangle: " + calc.Area(4.0, 3.0));
+        }
+    }
+}
---
+++ Polymorphism/Maths.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace oops
+{
+    class MathOperations
+    {
+        // Add methods
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        // Multiply methods
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Multiply(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+
+        // Subtract methods
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Subtract(int a, int b, int c)
+        {
+            return a - b - c;
+        }
+
+        // Divide methods
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Divide(int a, int b, int c)
+        {
+            return (a / b) / c;
+        }
+    }
+
+    class Maths
+    {
+        static void Main()
+        {
+            MathOperations math = new MathOperations();
+
+            Console.WriteLine("Add 2 numbers: " + math.Add(5, 3));
+            Console.WriteLine("Add 3 numbers: " + math.Add(5, 3, 2));
+
+            Console.WriteLine("Multiply 2 numbers: " + math.Multiply(4, 3));
+            Console.WriteLine("Multiply 3 numbers: " + math.Multiply(4, 3, 2));
+
+            Console.WriteLine("Subtract 2 numbers: " + math.Subtract(10, 4));
+            Console.WriteLine("Subtract 3 numbers: " + math.Subtract(10, 4, 2));
+
+            Console.WriteLine("Divide 2 numbers: " + math.Divide(20, 5));
+            Console.WriteLine("Divide 3 numbers: " + math.Divide(20, 5, 2));
+        }
+    }
+}
+
